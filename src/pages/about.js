22e0a19b82f0d0ef4555b857bd1580dd849cb2cf
Sean Kramer <seandominickramer@gmail.com>
2022-06-28 17:44:27 -0400

    Add styling in css modules

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+//  <Layout pageTitle='About'>
+//    <p>I am anonymous here.</p>
+//    </Layout>
+       <Layout pageTitle="About">
+       <p>I am anonymous here.</p>
+     </Layout>
+  )
+
+}
+
+export default AboutPage
